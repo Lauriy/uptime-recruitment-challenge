c5672332ccf439e94b7757ab0450f593313350c4
Lauri Elias <laurileet@gmail.com>
2017-12-13 23:20:58 +0200

    Add Mercury querying

--- web/index.php
+++ web/index.php
@@ -18,11 +18,27 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 // Our web handlers
 
 $app->get('/', function () use ($app) {
-    $app['monolog']->addDebug('logging output.');
     $feedContent = file_get_contents('https://flipboard.com/@raimoseero/feed-nii8kd0sz?rss');
     $xml = new SimpleXmlElement($feedContent);
+    //var_dump($xml);
 
-    return $app['twig']->render('index.twig');
+    return $app['twig']->render('index.twig', array(
+        'feed' => $xml
+    ));
+});
+
+$app->get('/mercury-parse/{urlBase64}', function ($urlBase64) use ($app) {
+    $opts = [
+        'http' => [
+            'method' => 'GET',
+            'header' => 'X-Api-Key: ' . getenv('MERCURY_API_KEY')
+        ]
+    ];
+
+    $context = stream_context_create($opts);
+    $mercuryParsed = file_get_contents('https://mercury.postlight.com/parser?url=' . base64_decode($urlBase64), false, $context);
+
+    return $mercuryParsed;
 });
 
 $app->run();
