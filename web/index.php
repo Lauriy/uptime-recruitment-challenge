a290535ec7dcd6da410747fa3f07fc5d291b0a33
Lauri Elias <laurileet@gmail.com>
2017-12-13 21:47:53 +0200

    Demo

--- web/index.php
+++ web/index.php
@@ -7,19 +7,22 @@ $app['debug'] = true;
 
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+    'monolog.logfile' => 'php://stderr',
 ));
 
 // Register view rendering
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+    'twig.path' => __DIR__ . '/views',
 ));
 
 // Our web handlers
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+$app->get('/', function () use ($app) {
+    $app['monolog']->addDebug('logging output.');
+    $feedContent = file_get_contents('https://flipboard.com/@raimoseero/feed-nii8kd0sz?rss');
+    $xml = new SimpleXmlElement($feedContent);
+
+    return $app['twig']->render('index.twig');
 });
 
 $app->run();
